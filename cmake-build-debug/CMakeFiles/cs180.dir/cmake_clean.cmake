32c21b7b638545e2772c27df766bacff42c4df46
etat002 <91437066+etat002@users.noreply.github.com>
2023-04-24 07:17:31 -0700

    Initial parse through records

---
+++ cmake-build-debug/CMakeFiles/cs180.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs180.dir/main.cpp.obj"
+  "cs180.exe"
+  "cs180.exe.manifest"
+  "cs180.pdb"
+  "libcs180.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs180.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
