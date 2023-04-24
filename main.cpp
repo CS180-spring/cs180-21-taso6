edce7ae369ae8e894cf936d2ff716f71cf272071
Billy Chau <bchau010@ucr.edu>
2023-04-24 04:07:00 -0700

    Initial Menu Created

--- main.cpp
+++ main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
 using namespace std;
 
+void login() {
+
+}
+void registration() {
+
+}
+void forgot() {
+  
+}
+
 int main() {
-  cout << "Hello World!";
+  int input;
+  cout << "_______________________________________________________________" << endl;
+  cout << "_____________________Account Creation Test_____________________" << endl;
+  cout << "______________________________Menu_____________________________" << endl;
+  cout << "_______________________________________________________________" << endl;
+  cout << "" << endl;
+  cout << "| 1. Login                    |" << endl;
+  cout << "| 2. Register                 |" << endl;
+  cout << "| 3. Forgot Password          |" << endl;
+  cout << "| 4. Exit                     |" << endl;
+  cout << "" << endl;
+  cin >> input;
+  cout << endl;
+
+  switch(input) {
+    case 1:
+      login();
+      break;
+    case 2:
+      registration();
+      break;
+    case 3:
+      forgot();
+      break;
+    case 4:
+      cout << "Theres no exit function yet lol" << endl;
+      break;
+    default:
+      system("cls");
+      cout << "Not a valid response, please try again." << endl;
+      main();
+  }
+
+
   return 0;
 }
